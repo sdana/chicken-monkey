81db81f698178873a76e594f29ef51575f2a485a
sdana <seth.dana@gmail.com>
2018-05-23 12:55:45 -0500

    added additional if statement for numbers divisible by 5 && 7

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 const ChickenMonkey = function() {
     for (let i=1; i<=100; i++) {
-        if (i % 5 === 0){
+        if (i % 5 === 0 && i % 7 === 0){
+            console.log("Chicken Monkey")
+        }
+        
+        else if (i % 5 === 0){
             console.log("Chicken")
         }
         else if (i % 7 === 0){
